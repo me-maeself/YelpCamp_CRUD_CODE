38fea2ec8fa0b4e01431d1143ff50c81a7f66338
me.maeself <me.maeself@gmail.com>
2024-01-21 03:14:11 +0700

    419, 420, 421: Init, home.ejs, campground models, makecampground route

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const Campground = require("./models/campground");
+
+main()
+	.then(() => {
+		console.log("(mongoose) Database Connected.");
+	})
+	.catch((err) => console.log(err));
+
+async function main() {
+	await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+}
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+	const camp = new Campground({
+		title: "My Backyard",
+		description: "Cheap Camping",
+	});
+	await camp.save();
+	res.send(camp);
+});
+
+app.listen(3000, () => {
+	console.log("(Express) Serving on 3000.");
+});
---
+++ readme.md
@@ -0,0 +1,29 @@
+This is project is based on **Web Development BootCamp 2023** by **Colt Steele** in **Udemy**. Section 39: YelpCamp: Campgrounds CRUD.
+`21/01/2023 by **me-maeself**`
+---
+TODO learn import export
+TODO learn path
+
+
+# Overview
+Features of the YelpCamp project:
+- Map
+- Locations
+- Authentication
+- Creation
+- Indexing
+
+# 419. How to access YelpCamp Code
+- the resource button direct us to a specific commit in github repo.
+
+# 420. Creating Basic Express App
+- npm init
+- npm i express ejs mongoose 
+
+# 421. Campground: Model Basic
+- making views
+  - home.ejs
+- making models
+  - campground
+- making home route
+- making makecampground route
